1cba401e45688183c685afb728f3b3e5d7896350
Camilo Romero <ca.romerom1@uniandes.edu.co>
2024-11-29 00:51:55 -0500

    fix: remove code duplication

--- src/game/Game.java
+++ src/game/Game.java
@@ -2,129 +2,75 @@ package game;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Game {
 
+	private String[][] board = { { "⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜" },
+			{ "⬜", "⬜", "⬜", "⬛", "⬛", "⬛", "⬜", "⬜" },
+			{ "⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬛", "⬜" },
+			{ "⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬛", "⬜" },
+			{ "⬜", "⬜", "⬜", "⬜", "⬜", "⬛", "⬜", "⬜" },
+			{ "⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬛", "⬜" },
+			{ "⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬛", "⬜" },
+			{ "⬜", "⬜", "⬜", "⬛", "⬛", "⬛", "⬜", "⬜" } };
+
+	private static int[][] blackTilesPattern1 = { { 1, 3 }, { 1, 4 }, { 1, 5 }, { 2, 2 }, { 2, 5 }, { 3, 6 }, { 4, 5 },
+			{ 5, 4 }, { 6, 3 }, { 7, 2 }, { 7, 3 }, { 7, 4 }, { 7, 5 }, { 7, 6 } };
+
+	private static int[][] blackTilesPattern2 = { { 1, 4 }, { 2, 3 }, { 3, 4 }, { 4, 4 }, { 5, 4 }, { 6, 4 }, { 7, 2 },
+			{ 7, 3 }, { 7, 4 }, { 7, 5 } };
+
+	private void printBoard() {
+		for (String[] row : this.board) {
+			for (String elem : row) {
+				System.out.print(" " + elem + " ");
+			}
+			System.out.println("");
+		}
+	}
+
+	private void wait(int ms) {
+		try {
+			Thread.sleep(ms);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		}
+	}
+
+	private void clearRow(String[] row) {
+		Arrays.fill(row, "⬜");
+	}
+
+	private void loadBlackTiles(int[][] blackTiles) {
+		for (int[] tile : blackTiles) {
+			board[tile[0]][tile[1]] = "⬛";
+		}
+	}
+
+	private void updateBoardPattern(int[][] pattern) {
+		wait(1000);
+
+		clearRow(this.board[0]);
+
+		loadBlackTiles(pattern);
+
+		printBoard();
+	}
+
 	public void play() {
 		int player = 0;
-		String[][] board = { { "⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜" },
-				{ "⬜", "⬜", "⬜", "⬛", "⬛", "⬛", "⬜", "⬜" },
-				{ "⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬛", "⬜" },
-				{ "⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬛", "⬜" },
-				{ "⬜", "⬜", "⬜", "⬜", "⬜", "⬛", "⬜", "⬜" },
-				{ "⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬛", "⬜" },
-				{ "⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬛", "⬜" },
-				{ "⬜", "⬜", "⬜", "⬛", "⬛", "⬛", "⬜", "⬜" } };
 
-		for (String[] row : board) {
-			for (String elem : row)
-				System.out.print(" " + elem + " ");
-			System.out.println("");
-		}
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			Thread.currentThread().interrupt();
-		}
+		printBoard();
 
-		for (int i = 0; i < board[0].length; i++)
-			board[0][i] = "⬜";
-		for (int i = 0; i < board[1].length; i++)
-			board[0][i] = "⬜";
-		for (int i = 0; i < board[2].length; i++)
-			board[0][i] = "⬜";
-		for (int i = 0; i < board[3].length; i++)
-			board[0][i] = "⬜";
-		for (int i = 0; i < board[4].length; i++)
-			board[0][i] = "⬜";
-		for (int i = 0; i < board[5].length; i++)
-			board[0][i] = "⬜";
-		for (int i = 0; i < board[6].length; i++)
-			board[0][i] = "⬜";
-		for (int i = 0; i < board[7].length; i++)
-			board[0][i] = "⬜";
-		board[1][3] = "⬛";
-		board[1][4] = "⬛";
-		board[1][5] = "⬛";
-		board[2][2] = "⬛";
-		board[2][5] = "⬛";
-		board[3][6] = "⬛";
-		board[4][5] = "⬛";
-		board[5][4] = "⬛";
-		board[6][3] = "⬛";
-		board[7][2] = "⬛";
-		board[7][3] = "⬛";
-		board[7][4] = "⬛";
-		board[7][5] = "⬛";
-		board[7][6] = "⬛";
+		updateBoardPattern(blackTilesPattern1);
 
-		for (String[] row : board) {
-			for (String r : row)
-				System.out.print(" " + r + " ");
-			System.out.println("");
-		}
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			Thread.currentThread().interrupt();
-		}
+		updateBoardPattern(blackTilesPattern2);
 
-		for (int i = 0; i < board[0].length; i++)
-			board[0][i] = "⬜";
-		for (int i = 0; i < board[1].length; i++)
-			board[0][i] = "⬜";
-		for (int i = 0; i < board[2].length; i++)
-			board[0][i] = "⬜";
-		for (int i = 0; i < board[3].length; i++)
-			board[0][i] = "⬜";
-		for (int i = 0; i < board[4].length; i++)
-			board[0][i] = "⬜";
-		for (int i = 0; i < board[5].length; i++)
-			board[0][i] = "⬜";
-		for (int i = 0; i < board[6].length; i++)
-			board[0][i] = "⬜";
-		for (int i = 0; i < board[7].length; i++)
-			board[0][i] = "⬜";
-		board[1][4] = "⬛";
-		board[2][3] = "⬛";
-		board[2][3] = "⬛";
-		board[3][4] = "⬛";
-		board[4][4] = "⬛";
-		board[5][4] = "⬛";
-		board[6][4] = "⬛";
-		board[7][2] = "⬛";
-		board[7][3] = "⬛";
-		board[7][4] = "⬛";
-		board[7][5] = "⬛";
+		wait(1000);
 
-		for (String[] row : board) {
-			for (String r : row)
-				System.out.print(" " + r + " ");
-			System.out.println("");
-		}
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			Thread.currentThread().interrupt();
-		}
-
-		for (int i = 0; i < board[0].length; i++)
-			board[0][i] = "⬜";
-		for (int i = 0; i < board[1].length; i++)
-			board[0][i] = "⬜";
-		for (int i = 0; i < board[2].length; i++)
-			board[0][i] = "⬜";
-		for (int i = 0; i < board[3].length; i++)
-			board[0][i] = "⬜";
-		for (int i = 0; i < board[4].length; i++)
-			board[0][i] = "⬜";
-		for (int i = 0; i < board[5].length; i++)
-			board[0][i] = "⬜";
-		for (int i = 0; i < board[6].length; i++)
-			board[0][i] = "⬜";
-		for (int i = 0; i < board[7].length; i++)
-			board[0][i] = "⬜";
+		clearRow(this.board[0]);
 
 		boolean playing = true;
 		while (playing) {
@@ -132,14 +78,10 @@ public class Game {
 				Random r = new Random();
 				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 				int newCar = r.nextInt(8);
-				board[0][newCar] = "🚙";
-				board[7][player] = "🚗";
+				this.board[0][newCar] = "🚙";
+				this.board[7][player] = "🚗";
 
-				for (String[] row : board) {
-					for (String a : row)
-						System.out.print(" " + a + " ");
-					System.out.println("");
-				}
+				printBoard();
 
 				// input
 				String key = br.readLine();
@@ -148,22 +90,22 @@ public class Game {
 					playing = false;
 					break;
 				} else if (key.equals("a") && player > 0) {
-					board[7][player] = "⬜";
+					this.board[7][player] = "⬜";
 					player -= 1;
 				} else if (key.equals("d") && player < 7) {
-					board[7][player] = "⬜";
+					this.board[7][player] = "⬜";
 					player += 1;
 				}
-				if (board[7][player].equals("🚙") || board[6][player].equals("🚙")) {
+				if (this.board[7][player].equals("🚙") || this.board[6][player].equals("🚙")) {
 					playing = false;
 					System.out.println("Perdiste!");
 
 					// move cars down
 					for (int i = 0; i < 7; i++)
-						board[7 - i] = board[6 - i];
+						this.board[7 - i] = this.board[6 - i];
 
-					for (int i = 0; i < board[0].length; i++)
-						board[0][i] = "⬜";
+					for (int i = 0; i < this.board[0].length; i++)
+						this.board[0][i] = "⬜";
 				}
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
